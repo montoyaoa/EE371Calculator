022ba366895b044eb1e4fc0fabd8157eb485f991
Osiel Montoya <montoyao@hawaii.edu>
2021-11-02 22:53:35 -1000

    renamed main file. added Makefile

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <list>
+
+#include "region.hpp"
+
+
+using namespace std;
+
+int main() {
+//prompt the user for input
+
+//assign input data to variables inside new objects (use a factory)
+
+//calculate results
+
+//display results
+
+}
